5c573aebd90b61e0cbd876f38f6b88e36ea295d1
Hugo <hugoabrantesd@gmail.com>
2022-03-25 20:09:41 -0300

    Criando projeto

---
+++ src/main/java/br/edu/fafic/cz_network/CzNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fafic.cz_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fafic/cz_network/CzNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fafic.cz_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
