3719c83a78f350348544ee27e1b07576c59a3b3c
Taylson Leite <taylson.leite@gmail.com>
2022-04-03 20:43:07 -0300

    Iniciando implementação do CRUD de pagina

---
+++ src/main/java/br/edu/fafic/cz_network/model/Pagina.java
@@ -0,0 +1,38 @@
+package br.edu.fafic.cz_network.model;
+
+
+import br.edu.fafic.cz_network.enums.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private String sobre;
+    private String foto;
+    @OneToMany
+    private List<Postagem> postagens;
+   // private List<String> curtidores; //TODO: MUDAR PARA O TIPO "Usuario"
+    private String usuario; //TODO: MUDAR PARA O TIPO "Usuario"
+    private String visibilidade;
+    private LocalDateTime dataHoraCriacao;
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+
+}
